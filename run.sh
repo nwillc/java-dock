0dcdc59230b68fcc3df7b61bea7ed2926abb202e
Nicholas Christopher <nwillc@gmail.com>
2017-01-15 21:48:11 -0500

    tweaks

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}" )" > /dev/null 2>&1 && pwd -P)"
 
 cd ${SCRIPT_DIR}
-docker run -d --name java-dock -p 8080:8080 nwillc/java-dock
+mkdir logs 2> /dev/null
+docker run -d --name java-dock -v $(pwd)/logs:/opt/service/logs -p 8080:8080 nwillc/java-dock
